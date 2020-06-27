f51ccf353b44bd793bf9bc0943dc90102bbe7dbb
victorisildur <victorisildur@163.com>
2020-06-27 23:26:38 +0800

    more data

---
+++ analyse_videos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+video_dir=./examples/media/basketball
+videos=`ls ${video_dir}`
+echo ${videos}
+for video in ${videos}
+do
+    echo "processing ${video}..."
+    ./build/examples/tutorial_api_cpp/my_code.bin -video_file=${video_dir}/${video} >  ${video}.data
+done
