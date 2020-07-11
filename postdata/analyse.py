ac6d1242df97f845f9312d3b60d88074f5f2ed44
victorisildur <victorisildur@163.com>
2020-07-11 09:35:36 +0800

    bugfix: try catch file reading code

--- postdata/analyse.py
+++ postdata/analyse.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     # 添加位置参数(positional arguments)
     parser.add_argument('--file', help='input a filename')
     args = parser.parse_args()
-    print(args.file)
+    filename = args.file
+    print(filename)
     t, xys = file_reader.read_x_y_score_list(args.file)
     ## python list -> np.array
     xy1s = np.array(xys[1])
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     xy9s = np.array(xys[9])
     xy10s = np.array(xys[10])
     xy11s = np.array(xys[11])
+    xy22s = np.array(xys[22])
     # 髋关节角度
     hip_angle = calc_angle(xy1s, xy10s, xy9s)
     # 大臂和躯干的角度
@@ -38,6 +40,8 @@ if __name__ == '__main__':
     elbow_angle = calc_angle(xy2s, xy4s, xy3s)
     # 膝盖角度
     knee_angle = calc_angle(xy9s, xy11s, xy10s)
+    # 脚踝角度
+    ankle_angle = calc_angle(xy10s, xy22s, xy11s)
     # 肩膀高度
     shoulder_height = np.subtract(720, xy2s[..., 1])
     # 手腕高度
@@ -46,20 +50,28 @@ if __name__ == '__main__':
     wrist_x = xy4s[..., 0]
     # 膝盖高度
     knee_height = np.subtract(720, xy10s[..., 1])
+    # 手腕高度
+    elbow_height = np.subtract(720, xy3s[..., 1])
     # 髋高
     hip_height = np.subtract(720, xy9s[..., 1])
+    # 脚踝高度
+    ankle_height = np.subtract(720, xy11s[..., 1])
     # 作图
     fig, ax = plt.subplots()
+    plt.title(filename)
     l0, = ax.plot(t, shoulder_height, label='shoulder height')
     l1, = ax.plot(t, wrist_height, label='wrist height')
     l7, = ax.plot(t, hip_height, label='hip height')
     l6, = ax.plot(t, knee_height, label='knee height')
+    l9, = ax.plot(t, elbow_height, label='elbow height')
+    l11, = ax.plot(t, ankle_height, label='ankle height')
     l2, = ax.plot(t, hip_angle, label='hip angle', linestyle='dashed')
     l3, = ax.plot(t, shoulder_angle, label='shoulder angle', linestyle='dashed')
     l4, = ax.plot(t, elbow_angle, label='elbow angle', linestyle='dashed')
     l5, = ax.plot(t, knee_angle, label='knee angle', linestyle='dashed')
     l8, = ax.plot(t, wrist_x, label='wrist x', linestyle='dashdot')
-    lines = [l0, l1, l6, l7, l2, l3, l4, l5, l8]
+    l10, = ax.plot(t, ankle_angle, label='ankle angle', linestyle='dashdot')
+    lines = [l0, l1, l6, l7, l9, l11, l2, l3, l4, l5, l8, l10]
     plt.legend()
     plt.subplots_adjust(left=0.2)
     plt.grid()
@@ -75,4 +87,5 @@ if __name__ == '__main__':
         plt.draw()
     check.on_clicked(func)
 
+    plt.gcf().canvas.set_window_title(filename)
     plt.show()
--- postdata/file_reader.py
+++ postdata/file_reader.py
@@ -8,16 +8,20 @@ def read_x_y_score_list(filename):
     while line:
         part = i % 25
         xyscore = line.split()
-        xyscore[0] = float(xyscore[0])
-        xyscore[1] = float(xyscore[1])
-        xyscore[2] = float(xyscore[2])
-        if part == 0:
-            t.append(i)
-        if len(xys) <= part:
-            xys.append([xyscore])
-        else:
-            xys[part].append(xyscore)
-        line = f.readline()
-        i+=1
+        try:
+            xyscore[0] = float(xyscore[0])
+            xyscore[1] = float(xyscore[1])
+            xyscore[2] = float(xyscore[2])
+            if part == 0:
+                t.append(i)
+            if len(xys) <= part:
+                xys.append([xyscore])
+            else:
+                xys[part].append(xyscore)
+            i += 1
+        except:
+            print()
+        finally:
+            line = f.readline()
     f.close()
     return t, xys
