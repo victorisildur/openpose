6bf440811781d1f0ebaef8786748820f5ac82268
victorisildur <victorisildur@163.com>
2020-07-13 08:44:53 +0800

    0712 data

--- analyse_videos.sh
+++ analyse_videos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 video_dir=./examples/media/basketball
-videos=`ls ${video_dir} -tr | grep 0711`
+data_dir=./postdata
+videos=`ls ${video_dir} -tr | grep 0712`
 echo ${videos}
 for video in ${videos}
 do
     echo "processing ${video}..."
-    ./build/examples/tutorial_api_cpp/my_code.bin -video_file=${video_dir}/${video} >  ${video}.data
+    ./build/examples/tutorial_api_cpp/my_code.bin -video_file=${video_dir}/${video} >  ${data_dir}/${video}.data
 done
